eec7165e365af7337dce09df3e22125033b9d8dd
Vishnu Dutt Pathak <vishnudutt.pathak@infineon.com>
2023-02-08 07:10:50 +0530

    Workflow orchestration -- prefect

---
+++ week2_workflow_orchestration/ingest-data.py
@@ -0,0 +1,60 @@
+
+import argparse
+import pandas as pd
+from sqlalchemy import create_engine
+from time import time
+import os
+from prefect import flow,task
+from prefect.tasks import task_input_hash
+from datetime import timedelta
+from prefect_sqlalchemy import SqlAlchemyConnector
+
+@task(log_prints=True, retries=3,cache_key_fn=task_input_hash,cache_expiration=timedelta(days=1))
+def extract_data(url):
+    output_file = "output.csv"
+    
+    if not os.path.exists("output.csv"):
+        os.system(f"curl {url} -o {output_file}")
+
+    trip_data = pd.read_parquet(f"{output_file}")
+    
+    chunk_trip_data = trip_data[:200000] # first 2 million data
+    return chunk_trip_data
+
+@task(log_prints=True, retries=3)
+def tranform_data(data):
+    assert isinstance(data,pd.DataFrame)
+    print(f"Pre: Missing passenge count : {data['passenger_count'].isin([0]).sum()}")
+    data = data[data['passenger_count']!=0] 
+    print(f"Post: Missing passenge count : {data['passenger_count'].isin([0]).sum()}")
+
+    return data
+
+@task(log_prints=True, retries=3)
+def ingest_data(user,password,host,port,db,table_name,data):
+    postgresurl = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+    engine = create_engine(postgresurl)
+    engine.connect()
+
+    data.to_sql(name=f'{table_name}',if_exists='replace',con = engine)
+
+# @flow(name="sub Flow")
+# def sub_flow(table_name):
+#     print(f"{table_name} has been created")
+
+@flow(name="ingest Flow")
+def main_flow(table_name):
+    user = "root"
+    password = "root"
+    host = "localhost"
+    port = 5431
+    db = "ny_taxi"
+    url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-11.parquet"
+
+    raw_data = extract_data(url)
+    clean_data = tranform_data(raw_data)
+    ingest_data (user,password,host,port,db,table_name,clean_data)
+
+if __name__=='__main__':
+    main_flow("yellow_taxi_data_22")
+    
